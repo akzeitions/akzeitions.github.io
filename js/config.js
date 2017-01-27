42aea0c2e59a76dd3486a8ac128c221a5365ae35
akzeitions <thiagocdeabreu@hotmail.com>
2017-01-27 11:59:25 -0200

    all

---
+++ js/config.js
@@ -0,0 +1,30 @@
+// Configuration Object
+var config = {};
+
+/*-------------------------------------------------------------------*/
+/*      1. Form Messages
+/*-------------------------------------------------------------------*/
+
+config.formMessages = {
+    sent          : 'Mensagem enviada com sucesso!',
+    fail          : 'Oops! Um gnomo leproso causou um erro.',
+    failEmail     : 'Email Inválido!',
+    emptyFields   : 'Oops! Por favor, complete os campos restantes!!'
+};
+
+/*-------------------------------------------------------------------*/
+/*      2. Google Map
+/*-------------------------------------------------------------------*/
+
+config.gmap = {
+    latitude    : '-23.600960',
+    longitude   : '-46.792426',
+    zoom        : 15,
+    marker      : 'img/marker.png'
+};
+
+/*-------------------------------------------------------------------*/
+/*      3. Easy Background System Settings
+/*-------------------------------------------------------------------*/
+
+config.backgrouns = {};
